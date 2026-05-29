c1f6d8fd8203bc784c8ddbc28afae6c28d9ee86b
prakriti1605 <prakritijain1205@gmail.com>
2026-05-29 17:07:05 +0530

    fixed cors for new branch

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://expense-tracker-production-cc99.up.railway.app/api/v1";
+const BASE_URL = "https://expense-tracker-production-cc99.up.railway.app/";
 
 const api = axios.create({ 
     baseURL: BASE_URL, 
